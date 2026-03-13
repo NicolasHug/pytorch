fa7fca9e2ce01cf7fb28a3aa0ef808fb8f4fd2df
Nicolas Hug <contact@nicolas-hug.com>
2026-03-13 10:25:27 +0000

    merge/parametrize into existing tests

--- test/test_nn.py
+++ test/test_nn.py
@@ -10635,7 +10635,7 @@ class TestNNDeviceType(NNTestCase):
 
     @parametrize_test("antialias", [True, False])
     @parametrize_test("num_channels", [3, 5])
-    @parametrize_test("mode", ["nearest", "nearest-exact", "bilinear", "bicubic"])
+    @parametrize_test("mode", ["nearest", "nearest-exact", "bilinear", "bicubic", "lanczos"])
     @parametrize_test("dtype", integral_types() + floating_types())
     @skipIfMPS  # Error message is wrong for some dtypes
     @onlyNativeDeviceTypes
@@ -10648,6 +10648,12 @@ class TestNNDeviceType(NNTestCase):
             if antialias:
                 raise SkipTest("Nearest mode does not have antialiasing")
             if dtype in (torch.uint8, ) + floating_types():
+                should_raise_runtime_error = False
+
+        elif mode == "lanczos":
+            if not antialias:
+                raise SkipTest("Lanczos mode requires antialias=True")
+            if dtype in floating_types() or (device == "cpu" and dtype == torch.uint8):
                 should_raise_runtime_error = False
 
         elif mode in ("bilinear", "bicubic"):
@@ -10683,7 +10689,7 @@ class TestNNDeviceType(NNTestCase):
     # Partially passes. NotImplementedError: aten::upsample_bicubic2d.out https://github.com/pytorch/pytorch/issues/77764
     @skipIfMPS
     @parametrize_test("memory_format", [torch.contiguous_format, torch.channels_last])
-    @parametrize_test("mode", ["bilinear", "bicubic"])
+    @parametrize_test("mode", ["bilinear", "bicubic", "lanczos"])
     @parametrize_test("antialias", [True, False])
     @parametrize_test("align_corners", [True, False])
     @parametrize_test("num_channels", [3, 5])
@@ -10708,13 +10714,19 @@ class TestNNDeviceType(NNTestCase):
         if torch.device(device).type == "cuda":
             raise SkipTest("CUDA implementation is not yet supporting uint8")
 
+        if mode == "lanczos":
+            if not antialias:
+                raise SkipTest("Lanczos mode requires antialias=True")
+            if align_corners:
+                raise SkipTest("Lanczos mode does not support align_corners=True")
+
         torch.manual_seed(0)
 
-        # - input range is set to [30, 220] for bicubic mode, because the bicubic kernel may create
-        #   [intermediate] values outside of the [0, 255] range, which need
-        #   to be clipped in uint8 path, but not in float path. This isn't
-        #   an issue with bilinear kernel.
-        input_range = (30, 220) if mode == "bicubic" else (0, 256)
+        # - input range is set to [30, 220] for bicubic and lanczos modes,
+        #   because these kernels may create [intermediate] values outside of
+        #   the [0, 255] range, which need to be clipped in uint8 path, but
+        #   not in float path. This isn't an issue with bilinear kernel.
+        input_range = (30, 220) if mode in ("bicubic", "lanczos") else (0, 256)
         input_ui8 = torch.randint(*input_range, size=(batch_size, num_channels, 400, 400), dtype=torch.uint8, device=device)
         input_ui8 = input_ui8.contiguous(memory_format=memory_format)
 
@@ -10752,6 +10764,7 @@ class TestNNDeviceType(NNTestCase):
         if mode == "bilinear":
             torch.testing.assert_close(output_f32, output_ui8.float(), rtol=0, atol=1)
         else:
+            # bicubic and lanczos
             diff = (output_f32 - output_ui8.float()).abs()
             self.assertLess(diff.max(), 15)
 
@@ -10835,59 +10848,6 @@ class TestNNDeviceType(NNTestCase):
         ], device=device, dtype=t_in.dtype).reshape(1, 3, 2, 2)
         t_out = F.interpolate(t_in, size=(2, 2), mode="lanczos", align_corners=False, antialias=True)
         self.assertEqual(expected_out, t_out)
-
-    @skipIfMPS
-    @parametrize_test("memory_format", [torch.contiguous_format, torch.channels_last])
-    @parametrize_test("num_channels", [3, 5])
-    @parametrize_test("output_size", [32, 600])
-    @parametrize_test("non_contig", [False, "sliced", "restrided"])
-    @parametrize_test("batch_size", [1, 5])
-    def test_upsamplingLanczos2d_consistency(
-        self,
-        device,
-        memory_format,
-        num_channels,
-        output_size,
-        non_contig,
-        batch_size,
-    ):
-        if torch.device(device).type == "cuda":
-            raise SkipTest("CUDA implementation is not yet supporting uint8")
-
-        torch.manual_seed(0)
-
-        input_ui8 = torch.randint(30, 220, size=(batch_size, num_channels, 400, 400), dtype=torch.uint8, device=device)
-        input_ui8 = input_ui8.contiguous(memory_format=memory_format)
-
-        if non_contig == "sliced":
-            input_ui8 = input_ui8[:, :, 10:-10, 10:-10]
-        elif non_contig == "restrided":
-            input_ui8 = input_ui8[:, :, ::2, ::2]
-
-        input_f32 = input_ui8.float()
-
-        output_f32 = F.interpolate(
-            input_f32, size=(output_size, output_size), mode="lanczos", align_corners=False, antialias=True
-        ).round().clip(0, 255)
-        output_ui8 = F.interpolate(
-            input_ui8, size=(output_size, output_size), mode="lanczos", align_corners=False, antialias=True
-        )
-
-        if non_contig is False:
-            self.assertTrue(input_ui8.is_contiguous(memory_format=memory_format))
-
-        diff = (output_f32 - output_ui8.float()).abs()
-        self.assertLess(diff.max(), 15)
-
-        threshold = 2
-        percent = 3
-        self.assertLess((diff > threshold).float().mean(), percent / 100)
-
-        threshold = 5
-        percent = 1
-        self.assertLess((diff > threshold).float().mean(), percent / 100)
-
-        self.assertLess(diff.mean(), 0.4)
 
     def test_upsamplingLanczos2d_errors(self, device):
         # 3D input (1D spatial) not supported
